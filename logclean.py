340492828e32b2c492c0b24c413a34336499a285
zoid <zoid.xyz@pm.me>
2026-02-17 20:49:47 -0600

    No comment.

--- logclean.py
+++ logclean.py
@@ -83,7 +83,7 @@ def stdin_parse(stream, join_part, purge_bots, bots):
 def parse_logs(logfile, join_part, purge_bots, bots, replace_logs, dry_run, quiet):
     logfile = pathlib.Path(logfile)
     logfile_name = logfile.stem
-    tmpfile = logfile.with_name(f"{logfile_name}.tmp")
+    tmpfile = logfile.with_name(f"{logfile.name}.tmp")
     original_filesize = logfile.stat().st_size
     lines_removed = 0
     with logfile.open('r', encoding='utf-8', errors='replace') as infile, \
